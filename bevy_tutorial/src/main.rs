e8fc38aee59e37763ade5ff65968e8e89f5c1da3
frankt716 <dailyuse716@gmail.com>
2024-09-06 08:05:17 +0200

    added a camera

--- bevy_tutorial/src/main.rs
+++ bevy_tutorial/src/main.rs
@@ -1,3 +1,21 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct CameraMarker;
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(10.0, 12.0, 16.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        },
+        CameraMarker,
+    ));
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup_camera)
+        .run();
 }
