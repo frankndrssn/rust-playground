d6e58a0484e2b304e18a20ca04f10c161dcb11c3
frankt716 <dailyuse716@gmail.com>
2024-09-08 19:18:47 +0200

    plane example

--- bevy_tutorial/src/main.rs
+++ bevy_tutorial/src/main.rs
@@ -1,21 +1,38 @@
+use bevy::math;
+use bevy::math::prelude::*;
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct CameraMarker;
-
-fn setup_camera(mut commands: Commands) {
-    commands.spawn((
-        Camera3dBundle {
-            transform: Transform::from_xyz(10.0, 12.0, 16.0).looking_at(Vec3::ZERO, Vec3::Y),
-            ..Default::default()
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(Plane3d {
+            normal: Dir3::Y,
+            half_size: Vec2::new(10.0f32, 10.0f32),
+        })),
+        material: materials.add(Color::srgb(0.3, 0.5, 0.3)),
+        ..default()
+    });
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
         },
-        CameraMarker,
-    ));
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup_camera)
+        .add_systems(Startup, setup)
         .run();
 }
